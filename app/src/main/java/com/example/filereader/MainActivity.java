22d43cb6436aee8e58087fdf3c6884d5f1ce1c2b
yushan33 <tanya870613@gmail.com>
2020-05-18 17:41:27 +0800

    1.增加刪除檔案的功能 2.增加icon

--- app/src/main/java/com/example/filereader/MainActivity.java
+++ app/src/main/java/com/example/filereader/MainActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -46,15 +47,14 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList <String> names;
-    private ArrayList <String> paths;
     private ArrayList <FileData> filesList;
     private static final int PERMISSION_REQUEST_STORAGE=30;
     private static final String ROOT_PATH = Environment.getExternalStorageDirectory().getPath();
     private static final String ROOT_PATH_NAME ="內部記憶體";
     private static final String ROOT_NAME = "回根目錄";
     private static final String PRE_LEVEL ="回上一層";
-    private static final String copy_dest_path= Environment.getExternalStorageDirectory()+"/file_reader/";
+    private static final String copy_dest_path= Environment.getExternalStorageDirectory()+"/File_reader/";
+    private static final String folder= Environment.getExternalStorageDirectory()+"/File_reader";
     private String target="";//目前位置
     private String nowPath="";
     private FileData fileData;
@@ -89,10 +89,12 @@ public class MainActivity extends AppCompatActivity {
     private void initData(){
 
         filesList = new ArrayList<>();//放入listview資料結構
-        names = new ArrayList<>();
-        paths = new ArrayList<>();
 
         tvNowPath=(TextView)findViewById(R.id.nowPath) ;
+        File ff = new File(folder);
+        if(!ff.exists() && !ff.isDirectory()){
+            ff.mkdir();
+        }
 
 
         getFileDirectory(ROOT_PATH);
@@ -103,17 +105,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void getFileDirectory(String path){
         filesList.clear();
-        paths.clear();
         if(!path.equals(ROOT_PATH)){
             //第0個位置放root
-            names.add(ROOT_NAME);
-            paths.add(ROOT_PATH);
             fileData=new FileData(ROOT_NAME,ROOT_PATH,false,4);
             filesList.add(fileData);
 
             //第1個位置放pre_level//前一個位置
-            names.add(PRE_LEVEL);
-            paths.add(new File(path).getParent());
             fileData=new FileData(PRE_LEVEL,new File(path).getParent(),false,4);
             filesList.add(fileData);
         }
@@ -179,12 +176,12 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 target = filesList.get(position).filePath;
                 if(target.equals(ROOT_PATH)){
-                    nowPath =paths.get(position);
+                    nowPath = filesList.get(position).filePath;
                     getFileDirectory(ROOT_PATH);
                     adapter.notifyDataSetChanged();
                     tvNowPath.setText(ROOT_PATH_NAME);
                 }else if (target.equals(PRE_LEVEL)){
-                    nowPath = paths.get(position);
+                    nowPath =  filesList.get(position).filePath;
                     getFileDirectory(new File(nowPath).getParent());
                     adapter.notifyDataSetChanged();
                     tvNowPath.setText(target);
@@ -294,6 +291,7 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             Message msg=new Message();
+                            msg.what=1;
                             handler.sendMessage(msg);
 
                         }
@@ -304,6 +302,40 @@ public class MainActivity extends AppCompatActivity {
                     break;
 
                 }
+                case R.id.delect:{
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(getString(R.string.delect_file))
+                            .setMessage(getString(R.string.delect_file_content))
+                            .setPositiveButton(getString(R.string.OK), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            for(int i=0 ;i<selectItems.size();i++){
+                                                if(!selectItems.get(i).equals(filesList.get(0)) && !selectItems.get(i).equals(filesList.get(1))){
+                                                    File file = new File(selectItems.get(i).filePath);
+                                                    file.delete();
+                                                }
+                                            }
+                                            Message msg = new Message();
+                                            msg.what=2;
+                                            handler.sendMessage(msg);
+
+                                        }
+                                    }).start();
+
+                                }
+                            })
+                            .setNegativeButton(getString(R.string.cancle), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(MainActivity.this, R.string.delect_file_not,Toast.LENGTH_LONG);
+                                }
+                            }).show();
+
+                    actionMode.finish();
+                }
 
 
             }
@@ -313,7 +345,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onDestroyActionMode(ActionMode actionMode) {
-            //selectItems.clear();
             adapter.setCheckable(false);
             adapter.notifyDataSetChanged();
 
@@ -342,8 +373,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage( Message msg) {
             super.handleMessage(msg);
-            Toast.makeText(MainActivity.this,R.string.copy_file_finish,Toast.LENGTH_LONG).show();
+            switch (msg.what){
+                case 1:
+                    Toast.makeText(MainActivity.this,R.string.copy_file_finish,Toast.LENGTH_LONG).show();
+                    break;
+                case 2:
+                    Toast.makeText(MainActivity.this,R.string.delect_file_finish,Toast.LENGTH_LONG).show();
+                    break;
+            }
 
+            getFileDirectory(target);
+            adapter.notifyDataSetChanged();
 
         }
     };
